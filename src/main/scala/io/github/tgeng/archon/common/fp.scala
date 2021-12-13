6450bc9d1e4ba6bee2e3312a711aa054e04cd3c9
Tianyu Geng <tgeng@google.com>
2021-12-13 13:05:26 -0800

    rename stuff

--- src/main/scala/io/github/tgeng/archon/common/fp.scala
+++ src/main/scala/io/github/tgeng/archon/common/fp.scala
@@ -3,54 +3,59 @@ package io.github.tgeng.archon.common
 import scala.collection.IterableFactory
 
 trait Functor[F[_]]:
-  extension[T] (f: F[T])
-    def map[S](g: T => S): F[S]
+  def map[T, S](f: F[T], g: T => S): F[S]
 
 trait Applicative[A[_]] extends Functor[A] :
   def pure[S](s: S): A[S]
 
-  extension[T] (a: A[T])
-    infix def <*>[S](f: A[T => S]): A[S]
+  def starApply[T, S](a: A[T], f: A[T => S]): A[S]
 
 trait Monad[M[_]] extends Applicative[M] :
-  extension[T] (m: M[T])
-    def flatMap[S](f: T => M[S]): M[S]
-    override def <*>[S](f: M[T => S]): M[S] =
-      for
-        t <- m
-        f <- f
-      yield
-        f(t)
+  def flatMap[T, S](m: M[T], f: T => M[S]): M[S]
+
+  override def starApply[T, S](m: M[T], f: M[T => S]): M[S] = flatMap(m, t => map(f, f => f(t)))
 
 trait Alternative[A[_]] extends Applicative[A] :
-  def empty[S] : A[S]
-  extension[T] (a: A[T])
-    infix def <|>(b: A[T]) : A[T]
+  def empty[S]: A[S]
+
+  def or[T](a: A[T], b: => A[T]): A[T]
 
 trait MonadPlus[M[_]] extends Monad[M] with Alternative[M]
 
-private def mapInternal[T, S](ts: Iterable[T], g: T => S) = ts.map(g)
-private def flatMapInternal[T, S](ts: Iterable[T], g: T => Iterable[S]) = ts.flatMap(g)
+extension[T, F[_]] (using env: Functor[F])(f: F[T])
+  def map[S](g: T => S): F[S] = env.map(f, g)
 
-given[CC[_] <: Iterable[_]] (using factory: IterableFactory[CC]): Monad[CC] with
-  def pure[S](s: S): CC[S] = factory.from(Iterable(s))
+extension[T, A[_]] (using env: Applicative[A])(a: A[T])
+  infix def <*>[S](f: A[T => S]): A[S] = env.starApply(a, f)
 
-  extension[T] (m: CC[T])
-    def map[S](g: T => S): CC[S] = mapInternal(m, g.asInstanceOf[Any => Any]).asInstanceOf[CC[S]]
-    def flatMap[S](f: T => CC[S]): CC[S] = flatMapInternal(m, f.asInstanceOf[Any => CC[Any]]).asInstanceOf[CC[S]]
+extension[T, M[_]] (using env: Monad[M])(m: M[T])
+  def flatMap[S](f: T => M[S]): M[S] = env.flatMap(m, f)
+
+extension[T, A[_]] (using env: Alternative[A])(a: A[T])
+  infix def <|>(b: A[T]): A[T] = env.or(a, b)
+
+given[CC[_] <: Iterable[_]] (using factory: IterableFactory[CC]): MonadPlus[CC] with
+  override def map[T, S](a: CC[T], f: T => S): CC[S] = a.map(f.asInstanceOf[Any => Any]).asInstanceOf[CC[S]]
+
+  override def pure[S](s: S): CC[S] = factory.from(Iterable(s))
+
+  override def flatMap[T, S](b: CC[T], f: T => CC[S]): CC[S] = b.flatMap(f.asInstanceOf[Any => IterableOnce[Any]]).asInstanceOf[CC[S]]
+
+  override def empty[S]: CC[S] = factory.empty
+
+  override def or[T](a: CC[T], b: => CC[T]): CC[T] = (a ++ b).asInstanceOf[CC[T]]
 
 given IterableFactory[List] = List
 given IterableFactory[Set] = Set
 
-given [L]: Monad[[R] =>> Either[L, R]] with
-  def pure[R](r: R): Either[L, R] = Right(r)
+given MonadPlus[Option] with
+  override def map[T, S](a: Option[T], g: T => S): Option[S] = a.map(g)
 
-  extension[R] (e: Either[L, R])
-    def map[S](g: R => S) : Either[L, S] = e match
-      case Right(r) => Right(g(r))
-      case Left(l) => Left(l)
+  override def pure[T](t: T): Option[T] = Option(t)
 
-    def flatMap[S](g: R => Either[L, S]) : Either[L, S] = e match
-      case Right(r) => g(r)
-      case Left(l) => Left(l)
+  override def flatMap[T, S](a: Option[T], g: T => Option[S]): Option[S] = a.flatMap(g)
+
+  override def empty[S]: Option[S] = Option.empty
+
+  override def or[T](a: Option[T], p: => Option[T]): Option[T] = a.orElse(p)
 
--- src/main/scala/io/github/tgeng/archon/parser/Parser.scala
+++ src/main/scala/io/github/tgeng/archon/parser/Parser.scala
@@ -2,15 +2,36 @@ package io.github.tgeng.archon.parser
 
 import io.github.tgeng.archon.common.{*, given}
 
-trait ParserT[I, +T, R[+_]]:
-  def parse(input: Seq[I]): (Seq[I], R[T])
+case class ParseError(message: String, targets: Seq[String])
 
-given [I, R[+_]]: MonadPlus[[T] =>> ParserT[I, T, R]] with
-  override def pure[S](s: S): ParserT[I, S, R] = ???
+given Ordering[ParseError] with
+  override def compare(x: ParseError, y: ParseError): Int = x.targets.size.compareTo(x.targets.size)
 
-  override def empty[S]: ParserT[I, S, R] = ???
+enum ParseResult[M[_] : MonadPlus, +T]:
+  case Success[M[_] : MonadPlus, T](result: M[T]) extends ParseResult[M, T]
+  case Failure[M[_] : MonadPlus, T](errors: Seq[ParseError]) extends ParseResult[M, T]
 
-  extension [T] (p: ParserT[I, T, R])
-    override def map[S](g: T => S): ParserT[I, S, R] = ???
-    override def flatMap[S](g: T => ParserT[I, S, R]): ParserT[I, S, R] = ???
-    override def <|>(q: ParserT[I, T, R]): ParserT[I, T, R] = ???
+given[M[_]] (using env: MonadPlus[M]): MonadPlus[[T] =>> ParseResult[M, T]] with
+  override def map[T, S](f: ParseResult[M, T], g: T => S): ParseResult[M, S] = ???
+
+  override def pure[T](t: T): ParseResult[M, T] = ParseResult.Success(env.pure(t))
+
+  override def flatMap[T, S](m: ParseResult[M, T], f: T => ParseResult[M, S]): ParseResult[M, S] = ???
+
+  override def empty[T]: ParseResult[M, T] = ParseResult.Failure(Seq())
+
+  override def or[T](a: ParseResult[M, T], b: => ParseResult[M, T]): ParseResult[M, T] = ???
+
+trait ParserT[I, +T, M[_] : MonadPlus]:
+  def parse(index: Int)(input: IndexedSeq[I]): (Int, ParseResult[M, T])
+
+//given[I, R[+_]] (using ParserResult[R]): MonadPlus[[T] =>> ParserT[I, T, R]] with
+//  override def pure[S](s: S): ParserT[I, S, R] = new ParserT[I, S, R] :
+//    override def parse(input: Seq[I]): (Seq[I], R[S]) = (input, pure(s))
+//
+//  override def empty[S]: ParserT[I, S, R] = ???
+//
+//  extension[T] (p: ParserT[I, T, R])
+//    override def fmap[S](g: T => S): ParserT[I, S, R] = ???
+//    override def bind[S](g: T => ParserT[I, S, R]): ParserT[I, S, R] = ???
+//    override def <|>(q: => ParserT[I, T, R]): ParserT[I, T, R] = ???
