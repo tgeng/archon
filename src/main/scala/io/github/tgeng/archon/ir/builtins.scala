642d8f7b705774d3f064bf9df6b825ff839987f4
Tianyu Geng <tgeng@google.com>
2022-04-22 13:17:19 -0700

    add builtins for computation type and fix bugs

--- src/main/scala/io/github/tgeng/archon/ir/builtins.scala
+++ src/main/scala/io/github/tgeng/archon/ir/builtins.scala
@@ -14,15 +14,15 @@ object Builtins:
   val TopQn = BuiltinType / "Top"
   val PureQn = BuiltinType / "Pure"
 
-//  val EqualityQn = BuiltinType / "Equality"
+  //  val EqualityQn = BuiltinType / "Equality"
   val EffectsQn = BuiltinType / "Effects"
   val LevelQn = BuiltinType / "Level"
   val HeapQn = BuiltinType / "Heap"
-//  val CellQn = BuiltinType / "Cell"
+  //  val CellQn = BuiltinType / "Cell"
 
-  val UnitTyQn = BuiltinType / "Unit"
-  val UnitTy = DataType(UnitTyQn)
-  val UnitQn = UnitTyQn / "Unit"
+  val UnitTypeQn = BuiltinType / "Unit"
+  val UnitType = DataType(UnitTypeQn)
+  val UnitQn = UnitTypeQn / "Unit"
   val Unit = Con(Name.Normal("Unit"))
 
 
@@ -48,43 +48,63 @@ object Builtins:
   val builtinDefinitions: Map[QualifiedName, (Definition, IndexedSeq[CheckedClause])] = Seq(
 
     /**
-     * Type : (level : LevelType) -> Type(level + 1, Top(level + 1))
+     * Type : (level : LevelType) -> Type(level + 1, Type(level, Top(l)))
      * {level : LevelType} |- level := .return Type(level, Top(l))
      */
     (Builtins.TypeQn,
       FunctionType(
         Binding(LevelType)(n"level"),
-        F(Type(USimpleLevel(LevelSuc(Var(0))), Top(USimpleLevel(LevelSuc(Var(0))))))
+        F(
+          Type(
+            USimpleLevel(LevelSuc(Var(0))),
+            Type(USimpleLevel(Var(0)), Top(USimpleLevel(Var(0))))
+          )
+        )
       ),
       IndexedSeq(
         CheckedClause(
           Binding(LevelType)(n"level") :: Nil,
           CPattern(PVar(0)) :: Nil,
           Return(Type(USimpleLevel(Var(0)), Top(USimpleLevel(Var(0))))),
-          F(Type(USimpleLevel(LevelSuc(Var(0))), Top(USimpleLevel(LevelSuc(Var(0))))))
+          F(
+            Type(
+              USimpleLevel(LevelSuc(Var(0))),
+              Type(USimpleLevel(Var(0)), Top(USimpleLevel(Var(0))))
+            )
+          )
         )
       )),
 
     /**
-     * SubTypeOf : (level : LevelType) -> Type(level, Top(level)) -> Type(level + 1, Top(level + 1))
-     * {level : LevelType, upperBound: Type(level, Top(level))} |- level upperBound := .return Type(level, upperBound)
+     * SubTypeOf : (level : LevelType) -> (upperBound : Type(level + 1, Top(level + 1))) -> Type(level + 1, Type(level, upperBound))
+     * {level : LevelType, upperBound: Type(level + 1, Top(level + 1))} |- level upperBound := .return Type(level, upperBound)
      */
     (Builtins.SubtypeOfQn,
       FunctionType(
         Binding(LevelType)(n"level"),
         FunctionType(
-          Binding(Type(USimpleLevel(Var(0)), Top(USimpleLevel(Var(0)))))(n"upperBound"),
-          F(Type(USimpleLevel(LevelSuc(Var(1))), Top(USimpleLevel(LevelSuc(Var(1))))))
+          Binding(
+            Type(
+              USimpleLevel(LevelSuc(Var(0))),
+              Top(USimpleLevel(LevelSuc(Var(0))))
+            )
+          )(n"upperBound"),
+          F(Type(USimpleLevel(LevelSuc(Var(1))), Type(USimpleLevel(Var(1)), Var(0))))
         )
       ),
       IndexedSeq(
         CheckedClause(
           Binding(LevelType)(n"level") ::
-            Binding(Type(USimpleLevel(Var(0)), Top(USimpleLevel(Var(0)))))(n"upperBound") ::
+            Binding(
+              Type(
+                USimpleLevel(LevelSuc(Var(0))),
+                Top(USimpleLevel(LevelSuc(Var(0))))
+              )
+            )(n"upperBound") ::
             Nil,
           CPattern(PVar(1)) :: CPattern(PVar(0)) :: Nil,
           Return(Type(USimpleLevel(Var(1)), Var(0))),
-          F(Type(USimpleLevel(LevelSuc(Var(1))), Top(USimpleLevel(LevelSuc(Var(1))))))
+          F(Type(USimpleLevel(LevelSuc(Var(1))), Type(USimpleLevel(Var(1)), Var(0))))
         )
       )
     ),
@@ -129,37 +149,37 @@ object Builtins:
       )
     ),
 
-//    /**
-//     * Equality : (level : LevelType) -> (ty : Type(level, Top(level)) -> (x : ty) -> (y : ty) -> Type(level, Equality(ty, x, y))
-//     * {level: Level, ty : Type... , x : ty, y : ty} |- _ ty x y = Equality(ty, x, y)
-//     */
-//    (
-//      Builtins.EqualityQn,
-//      FunctionType(
-//        Binding(LevelType)(n"level"),
-//        FunctionType(
-//          Binding(Type(USimpleLevel(Var(0)), Top(USimpleLevel(Var(0)))))(n"type"),
-//          FunctionType(
-//            Binding(Var(0))(n"x"),
-//            FunctionType(
-//              Binding(Var(1))(n"y"),
-//              F(Type(USimpleLevel(Var(3)), EqualityType(Var(2), Var(1), Var(0))))
-//            )
-//          )
-//        )
-//      ),
-//      IndexedSeq(
-//        CheckedClause(
-//          Binding(LevelType)(n"level") ::
-//            Binding(Type(USimpleLevel(Var(0)), Top(USimpleLevel(Var(0)))))(n"type") ::
-//            Binding(Var(0))(n"x") ::
-//            Binding(Var(1))(n"y") :: Nil,
-//          CPattern(PVar(2)) :: CPattern(PVar(1)) :: CPattern(PVar(0)) :: Nil,
-//          Return(EqualityType(Var(2), Var(1), Var(0))),
-//          F(Type(USimpleLevel(Var(3)), EqualityType(Var(2), Var(1), Var(0))))
-//        )
-//      )
-//    ),
+    //    /**
+    //     * Equality : (level : LevelType) -> (ty : Type(level, Top(level)) -> (x : ty) -> (y : ty) -> Type(level, Equality(ty, x, y))
+    //     * {level: Level, ty : Type... , x : ty, y : ty} |- _ ty x y = Equality(ty, x, y)
+    //     */
+    //    (
+    //      Builtins.EqualityQn,
+    //      FunctionType(
+    //        Binding(LevelType)(n"level"),
+    //        FunctionType(
+    //          Binding(Type(USimpleLevel(Var(0)), Top(USimpleLevel(Var(0)))))(n"type"),
+    //          FunctionType(
+    //            Binding(Var(0))(n"x"),
+    //            FunctionType(
+    //              Binding(Var(1))(n"y"),
+    //              F(Type(USimpleLevel(Var(3)), EqualityType(Var(2), Var(1), Var(0))))
+    //            )
+    //          )
+    //        )
+    //      ),
+    //      IndexedSeq(
+    //        CheckedClause(
+    //          Binding(LevelType)(n"level") ::
+    //            Binding(Type(USimpleLevel(Var(0)), Top(USimpleLevel(Var(0)))))(n"type") ::
+    //            Binding(Var(0))(n"x") ::
+    //            Binding(Var(1))(n"y") :: Nil,
+    //          CPattern(PVar(2)) :: CPattern(PVar(1)) :: CPattern(PVar(0)) :: Nil,
+    //          Return(EqualityType(Var(2), Var(1), Var(0))),
+    //          F(Type(USimpleLevel(Var(3)), EqualityType(Var(2), Var(1), Var(0))))
+    //        )
+    //      )
+    //    ),
 
     /**
      * Effects : Type(0, Effects)
@@ -211,6 +231,96 @@ object Builtins:
         )
       )
     ),
+
+    /**
+     * CType : (level : LevelType) -> (effects: EffectsType) -> CType(level + 1, CType(level, effects), Total)
+     * {level : LevelType, effects : EffectsType} level effects := CType(level, CTop(level), effects)
+     */
+    (
+      Builtins.CTypeQn,
+      FunctionType(
+        Binding(LevelType)(n"level"),
+        FunctionType(
+          Binding(EffectsType)(n"effects"),
+          CType(
+            USimpleLevel(LevelSuc(Var(1))),
+            CType(USimpleLevel(Var(1)), CTop(USimpleLevel(Var(1)), Var(0)))
+          )
+        )
+      ),
+      IndexedSeq(
+        CheckedClause(
+          Binding(LevelType)(n"level") :: Binding(EffectsType)(n"effects") :: Nil,
+          CPattern(PVar(1)) :: CPattern(PVar(0)) :: Nil,
+          CType(USimpleLevel(Var(1)), CTop(USimpleLevel(Var(1)), Var(0))),
+          CType(
+            USimpleLevel(LevelSuc(Var(1))),
+            CType(USimpleLevel(Var(1)), CTop(USimpleLevel(Var(1)), Var(0)))
+          )
+        )
+      )
+    ),
+
+    /**
+     * CSubTypeOf : (level : LevelType) -> (effects: EffectsType) -> (upperBound : U(CType(level + 1, CTop(level + 1, effects)))) -> CType(level + 1, .force upperBound, Total)
+     * {level : LevelType, effects: Effects, upperBound: U(CType(level + 1, CTop(level + 1, effects)))} |- level effects upperBound := CType(level, .force upperBound, effects)
+     */
+    (Builtins.SubtypeOfQn,
+      FunctionType(
+        Binding(LevelType)(n"level"),
+        FunctionType(
+          Binding(EffectsType)(n"effects"),
+          FunctionType(
+            Binding(
+              U(CType(USimpleLevel(LevelSuc(Var(1))), CTop(USimpleLevel(LevelSuc(Var(1))), Var(0))))
+            )(n"upperBound"),
+            CType(
+              USimpleLevel(LevelSuc(Var(2))),
+              CType(USimpleLevel(Var(2)), Force(Var(0)), Var(1))
+            )
+          )
+        )
+      ),
+      IndexedSeq(
+        CheckedClause(
+          Binding(LevelType)(n"level") ::
+            Binding(EffectsType)(n"effects") ::
+            Binding(
+              U(CType(USimpleLevel(LevelSuc(Var(1))), CTop(USimpleLevel(LevelSuc(Var(1))), Var(0))))
+            )(n"upperBound") ::
+            Nil,
+          CPattern(PVar(2)) :: CPattern(PVar(1)) :: CPattern(PVar(0)) :: Nil,
+          CType(USimpleLevel(Var(2)), Force(Var(0)), Var(1)),
+          CType(
+            USimpleLevel(LevelSuc(Var(2))),
+            CType(USimpleLevel(Var(2)), Force(Var(0)), Var(1))
+          )
+        )
+      )
+    ),
+
+    /**
+     * CTop : (level : LevelType) -> (effects : EffectsType) -> CType(level, CTop(level, effects))
+     * {level : LevelType, effects : EffectsType} |- level effects := CTop(level, effects)
+     */
+    (
+      Builtins.CTopQn,
+      FunctionType(
+        Binding(LevelType)(n"level"),
+        FunctionType(
+          Binding(EffectsType)(n"effects"),
+          CType(USimpleLevel(Var(1)), CTop(USimpleLevel(Var(1)), Var(0)))
+        ),
+      ),
+      IndexedSeq(
+        CheckedClause(
+          Binding(LevelType)(n"level") :: Binding(EffectsType)(n"effects") :: Nil,
+          CPattern(PVar(1)) :: CPattern(PVar(0)) :: Nil,
+          CTop(USimpleLevel(Var(1)), Var(0)),
+          CType(USimpleLevel(Var(1)), CTop(USimpleLevel(Var(1)), Var(0)))
+        )
+      )
+    )
 
   ).map { case (qn, ty, clauses) => (qn, (new Definition(qn)(ty), clauses)) }.toMap
 
