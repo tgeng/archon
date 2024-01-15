4e3ee72c61ada34181c07c8f56b16064f29dd860
Tianyu Geng <aipser.3d@gmail.com>
2024-01-15 14:11:47 -0800

    move EqDecidability

--- src/main/scala/com/github/tgeng/archon/core/ir/term.scala
+++ src/main/scala/com/github/tgeng/archon/core/ir/term.scala
@@ -27,6 +27,25 @@ object Binding:
   */
 type Arguments = List[VTerm]
 
+/** A type is eqDecidable if the equality of its inhabitants can be efficiently determined at runtime. That is, a type
+  * is not eqDecidable if its inhabitants
+  *   - contain thunks because in general there is no way to decide equality of computations
+  *   - contain erased parts whose equality can not be derived from non-erased parts and hence there is no way to decide
+  *     equality at runtime
+  *
+  * The primary purpose of this type is to limit what types are allowed as args to effects. Efficient methods of
+  * deciding equality is needed for runtime handler resolution.
+  *
+  * In future, it's also likely that we can introduce some built-in hashing data structure that can leverage this as a
+  * bound for the key type.
+  */
+enum EqDecidability:
+  case EqDecidable, EqUnknown
+
+  infix def |(that: EqDecidability): EqDecidability = (this, that) match
+    case (EqDecidable, _) | (_, EqDecidable) => EqDecidable
+    case _                                   => EqUnknown
+
 enum ControlMode extends Ordered[ControlMode]:
   /** No continuation is captured, execution simply progresses. The continuation usage can be U0, U1, or UAff.
     */
