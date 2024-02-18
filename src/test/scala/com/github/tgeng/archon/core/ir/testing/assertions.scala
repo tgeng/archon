e9c9342b4a81ad42f4c47d1e206871a2c19ff088
Tianyu Geng <aipser.3d@gmail.com>
2024-02-18 14:17:18 -0800

    add more literal tests

--- src/test/scala/com/github/tgeng/archon/core/ir/testing/assertions.scala
+++ src/test/scala/com/github/tgeng/archon/core/ir/testing/assertions.scala
@@ -9,7 +9,14 @@ def assertType
   (using Σ: Signature)
   (using ctx: TypingContext)
   : Unit = {
-  checkType(t, ty)
+  try {
+    checkType(t, ty)
+  } catch {
+    case e: IrError => {
+      enableDebugging
+      checkType(t, ty)
+    }
+  }
 }
 
 def assertNotType
@@ -19,6 +26,8 @@ def assertNotType
   (using ctx: TypingContext)
   : Unit = {
   try {
+    checkType(t, ty)
+    enableDebugging
     checkType(t, ty)
     fail(s"Expected type mismatch.")
   } catch {
--- src/test/scala/com/github/tgeng/archon/core/ir/typing/BasicTypeCheckSpec.scala
+++ src/test/scala/com/github/tgeng/archon/core/ir/typing/BasicTypeCheckSpec.scala
@@ -1,6 +1,9 @@
 package com.github.tgeng.archon.core.ir.typing
 
 import com.github.tgeng.archon.core.ir.*
+import com.github.tgeng.archon.core.ir.EqDecidability.{EqDecidable, EqUnknown}
+import com.github.tgeng.archon.core.ir.HandlerType.*
+import com.github.tgeng.archon.core.ir.Usage.*
 import com.github.tgeng.archon.core.ir.VTerm.*
 import com.github.tgeng.archon.core.ir.testing.*
 import org.scalatest.freespec.AnyFreeSpec
@@ -10,7 +13,37 @@ class BasicTypeCheckSpec extends AnyFreeSpec:
   given Signature = SimpleSignature()
   given TypingContext = TypingContext()
   given SourceInfo = SourceInfo.SiEmpty
-  "In empty context and signature" - {
-    "Check literals" in:
+  "in empty context and signature" - {
+    "check level literals" in:
       assertType(LevelLiteral(0), LevelType())
+      assertType(LevelLiteral(1), LevelType())
+      assertNotType(LevelLiteral(1, 1), LevelType())
+      assertType(LevelLiteral(1, 1), LevelType(LevelLiteral(1, 1)))
+      assertNotType(UsageLiteral(U0), LevelType())
+
+    "check usage literals" in:
+      assertType(UsageLiteral(U0), UsageType())
+      assertType(UsageLiteral(U1), UsageType())
+      assertType(UsageLiteral(UAny), UsageType(Some(UsageLiteral(U1))))
+      assertType(UsageLiteral(UAny), UsageType(Some(UsageLiteral(U0))))
+      assertType(UsageLiteral(UAny), UsageType(Some(UsageLiteral(UAff))))
+      assertType(UsageLiteral(UAny), UsageType(Some(UsageLiteral(URel))))
+      assertType(UsageLiteral(UAff), UsageType(Some(UsageLiteral(U0))))
+      assertType(UsageLiteral(UAff), UsageType(Some(UsageLiteral(U1))))
+      assertType(UsageLiteral(URel), UsageType(Some(UsageLiteral(U1))))
+      assertNotType(UsageLiteral(U1), UsageType(Some(UsageLiteral(UAny))))
+      assertNotType(UsageLiteral(U1), UsageType(Some(UsageLiteral(URel))))
+      assertNotType(UsageLiteral(U1), UsageType(Some(UsageLiteral(UAff))))
+      assertNotType(UsageLiteral(U1), UsageType(Some(UsageLiteral(U0))))
+      assertNotType(LevelLiteral(1), UsageType())
+
+    "check eq decidability literals" in:
+      assertType(EqDecidabilityLiteral(EqDecidable), EqDecidabilityType())
+      assertType(EqDecidabilityLiteral(EqUnknown), EqDecidabilityType())
+      assertNotType(LevelLiteral(1), EqDecidabilityType())
+
+    "check handler type literals" in:
+      assertType(HandlerTypeLiteral(Simple), HandlerTypeType())
+      assertType(HandlerTypeLiteral(Complex), HandlerTypeType())
+      assertNotType(LevelLiteral(1), HandlerTypeType())
   }
