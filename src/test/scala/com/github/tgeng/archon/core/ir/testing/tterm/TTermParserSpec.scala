b93c61c7b2965a625fb59b617d8861cc48ac399a
Tianyu Geng <aipser.3d@gmail.com>
2024-03-17 22:32:13 -0700

    fix test pretty printer; add tDef

---
+++ src/test/scala/com/github/tgeng/archon/core/ir/testing/tterm/TTermParserSpec.scala
@@ -0,0 +1,43 @@
+package com.github.tgeng.archon.core.ir.testing.tterm
+
+import com.github.tgeng.archon.common.FileBasedFreeSpec
+import com.github.tgeng.archon.core.common.{Name, QualifiedName}
+import os.Path
+
+import scala.collection.mutable
+
+object TTermParserSpec:
+  def tTermPprint: pprint.PPrinter =
+    val visited = mutable.Set.empty[TTerm]
+    def p: pprint.PPrinter = pprint.copy(
+      additionalHandlers = {
+        case qn: QualifiedName => pprint.Tree.Literal(s"qn\"${qn.toString}\"")
+        case n: Name           => pprint.Tree.Literal(s"n\"${n.toString}\"")
+        case t: TTerm if !visited.contains(t) =>
+          visited += t
+          pprint.Tree
+            .Infix(
+              p.treeify(t, false, true),
+              "@",
+              pprint.Tree.Literal(t.sourceInfo.toString),
+            )
+      },
+    )
+    p
+
+class TTermParserSpec extends FileBasedFreeSpec:
+
+  override protected def runTestImpl(testDir: Path): Unit =
+    val inputPath = testDir / "input.tterm"
+    val actual = TTermParserSpec.tTermPprint.apply(Parser.parse(inputPath)).plainText
+    val outputPath = testDir / "output.txt"
+    val expected = if os.exists(outputPath) then os.read(outputPath) else ""
+    if actual != expected then
+      os.write.over(outputPath, actual)
+      fail(s"Expected:\n$expected\nActual:\n$actual")
+
+  "simple id" in:
+    runTest()
+
+  "simple_def" in:
+    runTest()
--- src/test/scala/com/github/tgeng/archon/core/ir/testing/tterm/parser.scala
+++ src/test/scala/com/github/tgeng/archon/core/ir/testing/tterm/parser.scala
@@ -1,5 +1,7 @@
 package com.github.tgeng.archon.core.ir.testing.tterm
 
+import com.github.tgeng.archon.core.common.QualifiedName
+import com.github.tgeng.archon.core.common.QualifiedName.Root
 import com.github.tgeng.archon.core.ir.{SourceInfo, TokenRange}
 import fastparse.NoWhitespace.{*, given}
 import fastparse.{*, given}
@@ -14,12 +16,15 @@ class Parser(val text: String, val path: Option[Path]) {
     })
   private def id[$: P]: P[String] = P((CharIn("a-zA-Z_") ~ CharIn("a-zA-Z0-9_").rep).!)
   private def tId[$: P]: P[TTerm] = PT(id)(TTerm.TId(_))
+  private def tDef[$: P]: P[TTerm] =
+    PT(("." ~ id).rep(1).!)(s => TTerm.TDef(QualifiedName.from(s.drop(1))))
+  private def tTerm[$: P]: P[TTerm] = P(tId | tDef)
 }
 
 object Parser:
   def parse(path: Path): TTerm =
     val text = os.read(path)
     val parser = new Parser(text, Some(path))
-    fastparse.parse(text, parser.tId(using _)) match
+    fastparse.parse(text, parser.tTerm(using _)) match
       case Parsed.Success(value, _)    => value
       case Parsed.Failure(_, _, extra) => throw new RuntimeException(extra.trace().longMsg)
